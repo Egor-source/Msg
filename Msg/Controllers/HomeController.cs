77c5b820b508d7e99dbc7054c90acfc3f8fb5f6c
Egor-source <egor@DESKTOP-784KJVG.localdomain>
2020-05-26 01:32:23 +0300

    Final version of people search

--- Msg/Controllers/HomeController.cs
+++ Msg/Controllers/HomeController.cs
@@ -42,17 +42,34 @@ namespace Msg.Controllers
             return RedirectToAction("Login","Account");
         }
 
+        /// <summary>
+        /// Поиск человека
+        /// </summary>
+        /// <param name="man">Искомый человек</param>
+        /// <param name="id">id пользователя</param>
+        /// <returns>JSON строку с возможными вариантами поиска</returns>
         public string GetPeople(string man,string id)
         {
+            //Массив искомых строк
             string[] requered = man.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
 
-            string json=null;
+            //Если не было передано строк
+            if (requered.Length<=0)
+                return "";
 
-            foreach (var i in requered)
+            var first = requered[0];
+
+            //Ищет совпадения по первому элементу массива requered
+            var users = from user in db.Users where user.Id != id && (user.Name.Contains(first) || user.Surname.Contains(first)) select new { user.Name, user.Surname, user.Photo };
+            for(int i=1;i<requered.Length;i++)
             {
-                var users = from user in db.Users where user.Id != id&& (user.Name.Contains(i) || user.Surname.Contains(i)) select new {user.Name,user.Surname,user.Photo};
-               json+=JsonConvert.SerializeObject(users);
+                //Текущая строка поиска
+                var current = requered[i];
+                //Ищет совпадения по текущей строке поиска
+                users =from user in users where user.Name.Contains(current) || user.Surname.Contains(current) select user;
             }
+            //Конвертирует пользователей,удовлетворяющих запросу,в JSON строку
+            string json = JsonConvert.SerializeObject(users);
 
             return json;
         }
--- Msg/Scripts/PeopleSearch.js
+++ Msg/Scripts/PeopleSearch.js
@@ -1,34 +1,46 @@
 ﻿
-
+//срабатывает при нажатии на клавишу в input .search
 function Search(key,id) {
-
-    if (key >= 48 && key <= 90 || key == 13 || key==8 || key >= 186 && key <= 222) {
+    //Проверяет код нажатой клавиши
+    if (key >= 48 && key <= 90 || key == 13 || key == 8 || key >= 186 && key <= 222) {
+        //Перадает значения input .search
         let man = $(".search").val();
+        //Очищает элемент .users
         $(".users").empty();
+        //Отправляет ajax запрос
         $.ajax({
+            //Метод обработки
             url: "Home/GetPeople",
+            //Тип запроса
             type: "POST",
-            data:{ man: man, id: id },
+            //Передаваеймые в метод переменные
+            data: { man: man, id: id },
+            //Тип передаваемых данных
             dataType: "html",
+            //Метод,срабатывающий перед отправкой запроса
             beforeSend: Before,
+            //Медот,срабатывающий после выполнения запроса 
             success: Success
         });
     }
 }
-
 function Before() {
     $(".status").text("Поиск...");
 }
 
 function Success(users) {
-
+    //Очищает элемент .status
     $(".status").empty();
+    //Конвертирует JSON строку в js объект
     users = jQuery.parseJSON(users);
 
+    //Проверяет были ли возвращены значения
     if (users.length === 0) {
         $(".status").text("Пользователь не найден");
+        return;
     }
 
+    //Генерирует представления для найденных пользователей
     for (var i = 0; i < users.length; i++) {
         $(".users").append($('<div class="user"><img src="/Content/Photo/' + users[i].Photo + '" class="UserPhoto" /> <p class="">' + users[i].Name + '</p> <p class="">' + users[i].Surname + '</p></div > '));   
                          
