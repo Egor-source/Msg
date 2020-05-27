04b9bcef56ff1d5dbf8d8ed80f294dfda92f7b57
Egor-source <egor@DESKTOP-784KJVG.localdomain>
2020-05-27 20:30:39 +0300

    Added connection and disconnection methods

---
+++ Msg/Models/Home/MainHubModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Msg.Models.Home
+{
+    public class MainHubModel
+    {
+        public List<string> ConnectionId { get; set; }
+
+        public string UserId { get; set; }
+
+        public int TabsCount { get; set; }
+
+        public MainHubModel(string connectionId,string userId)
+        {
+            ConnectionId = new List<string>();
+            ConnectionId.Add(connectionId);
+            UserId = userId;
+            TabsCount = 1;
+        }
+
+
+        public void AddTab(string connectionId)
+        {
+            ConnectionId.Add(connectionId);
+            TabsCount++;
+        }
+
+        public void RemoveTab(string connectionId)
+        {
+            ConnectionId.Remove(connectionId);
+            TabsCount--;
+        }
+    }
+}
---
+++ Msg/Scripts/MainHub.js
@@ -0,0 +1,23 @@
+﻿$(document).ready(function () {
+
+    var main = $.connection.mainHub;
+
+    var cookie = $.cookie("User").split('Name');
+
+    var Id = cookie[0].split("=");
+
+    main.client.friendshepRequest = function (jsonUser) {
+
+        var sendingRequestUser = JSON.parse(jsonUser);
+    }
+
+    $.connection.hub.start().done(function () {
+
+        main.server.connect(Id[1]);
+    });
+
+});
+
+function GetFriendRequest(user) {
+    $(".newFriends").append($('<div class="user"><input type="hidden" value="' + user.Id + '" class="id" /><img src="/Content/Photo/' + user.Photo + '" class="UserPhoto" /> <p class="">' + user.Name + '</p> <p class="">' + user.Surname + '</p><button>Добавить</button></div > '));
+}
