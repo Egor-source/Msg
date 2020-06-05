116633a9231dfd75629f5be363bead540a2c9c5a
Egor-source <egor@DESKTOP-784KJVG.localdomain>
2020-06-05 10:10:51 +0300

    Page master design has become responsive

---
+++ Msg/obj/Release/Package/PackageTmp/Scripts/MasterPageScripts.js
@@ -0,0 +1,20 @@
+﻿
+
+function MenuButtonAction() {
+    $('.hedder__content__authUser__menu').toggleClass('hedder__content__authUser__menu__active');
+}
+
+function MenuButtonAction2() {
+
+    $('.hedder__content__menu__icon').removeClass('hedder__content__menu__icon__active');
+    $('.hedder__content__menu').removeClass('hedder__content__menu__active');
+    $('.hedder__content__authUser__menu').removeClass('hedder__content__authUser__menu__active');
+}
+
+
+function MenuClick () {
+
+    $('.hedder__content__menu__icon').toggleClass('hedder__content__menu__icon__active');
+    $('.hedder__content__menu').toggleClass('hedder__content__menu__active');
+    $('.hedder__content__authUser__menu').toggleClass('hedder__content__authUser__menu__active');
+}
